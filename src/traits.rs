53f35e6f2173d90bbd56ddf8c0ba87dab743716d
Krishnanand V P <vpkrishnanand@gmail.com>
2022-08-21 23:06:56 +0530

    Add traits 🌿

---
+++ src/traits.rs
@@ -0,0 +1,16 @@
+pub struct NewsArticle {
+    pub author: String,
+    pub headlin: String,
+    pub content: String,
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
