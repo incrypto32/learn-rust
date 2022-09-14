d97a6010d3bac95b797445f1eaa630821a2ab8b5
Krishnanand V P <vpkrishnanand@gmail.com>
2022-09-14 07:30:55 +0530

    closure : Added cacher struct

--- src/closures.rs
+++ src/closures.rs
@@ -4,8 +4,37 @@ use std::{thread, time::Duration};
 
 pub fn run() {}
 
-struct  Cacher{
-    
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        let cacher = Cacher {
+            calculation: calculation,
+            value: None,
+        };
+
+        return cacher;
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
