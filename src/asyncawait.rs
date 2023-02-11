e0744a05627207927983d27b437d253e6b9b041b
Krishnanand V P <vpkrishnanand@gmail.com>
2023-02-11 18:04:45 +0530

    Add tokio handles

--- src/asyncawait.rs
+++ src/asyncawait.rs
@@ -23,21 +23,39 @@
 // }
 ////////////////////////////////////////////////////////
 
+use std::time::Duration;
+
+use tokio::time::sleep;
+
 // For top most futures we need some code which will manually poll the future
 // Programming language like C++ and Java have a runtime which will do this for us
 // Rust does not have a runtime so we need to write this code ourselves
 // So inorder to run async code we need to use a runtime like tokio or async-std
+
+
 pub async fn run() {
-    my_function().await;
+    let mut handles = vec![];
+
+    for i in 0..2 {
+        let handle = tokio::spawn(async move {
+            my_function(i).await;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
 }
-async fn my_function() {
-    println!("Im an async function");
+async fn my_function(i: i32) {
+    println!("[{i}] Im an async function");
     let s1 = read_from_database().await;
-    println!("s1 : {}", s1);
+    println!("[{i}] s1 : {}", s1);
     let s2 = read_from_database().await;
-    println!("s2 : {}", s2);
+    println!("[{i}] s2 : {}", s2);
 }
 
 async fn read_from_database() -> String {
+    sleep(Duration::from_millis(50)).await;
     return "DB data".to_string();
 }
